42bf181df9295b30eb792120e26f866e7c3cf992
Sailor-anand <147530396+Sailor-anand@users.noreply.github.com>
2024-11-25 21:15:06 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+const express=require('express');
+const app=express();
+const PORT=process.env.PORT || 8000;
+app.get('/',(req,res)=>{
+    res.send('hello docker');
 });
-
-app.listen(5000,() => {
-    console.log('Server is running on port 5000');
+app.listen(8000,'0.0.0.0',()=>{
+    console.log('running');
 });
